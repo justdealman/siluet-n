65b77828642c1e0c4875001e5f70a922584356b6
justdealman <justdealman@gmail.com>
2018-09-13 04:16:40 +0700

    gotop action + mobile show

--- js/script.js
+++ js/script.js
@@ -391,4 +391,11 @@ $(function() {
 			}
 		}        
 	});
+	
+	$('.to-top').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').stop().animate({
+			scrollTop: 0
+		}, 800);
+	})
 });
